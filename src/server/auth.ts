2a7812e616adeca47c6e4028a78621cbcac602b3
riso.schwarcz <riso.schwarcz@gmail.com>
2023-10-08 13:17:30 +0200

    feat: auth for yt api, set access token to session

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,14 +1,15 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { type GetServerSidePropsContext } from "next";
 import {
   getServerSession,
   type DefaultSession,
   type NextAuthOptions,
+  type TokenSet,
 } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 import { env } from "~/env.mjs";
-import { db } from "~/server/db";
+// import { db } from "~/server/db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -23,6 +24,7 @@ declare module "next-auth" {
       // ...other properties
       // role: UserRole;
     };
+    token: TokenSet;
   }
 
   // interface User {
@@ -31,21 +33,47 @@ declare module "next-auth" {
   // }
 }
 
+const scopes = [
+  "https://www.googleapis.com/auth/youtube",
+  "openid",
+  "email",
+  "profile",
+];
+
 export const authOptions: NextAuthOptions = {
-  callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
+  session: {
+    strategy: "jwt",
   },
-  adapter: PrismaAdapter(db),
+  callbacks: {
+    jwt: ({ token, account }) => {
+      if (account) {
+        token = {
+          ...token,
+          access_token: account.access_token,
+          scope: account.scope,
+        };
+      }
+      return token;
+    },
+    session: ({ session, token }) => {
+      // console.log(token, "TOKEN");
+      session = {
+        ...session,
+        token,
+      };
+      return session;
+    },
+  },
+  // adapter: PrismaAdapter(db),
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: scopes.join(" "),
+        },
+      },
     }),
   ],
 };
