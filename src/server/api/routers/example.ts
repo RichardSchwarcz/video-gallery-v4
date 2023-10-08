30353b12a1d1979c1f1eb7c3e38c1601b49745e7
riso.schwarcz <riso.schwarcz@gmail.com>
2023-10-08 13:18:21 +0200

    feat: create get youtube videos procedure

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -22,4 +22,28 @@ export const exampleRouter = createTRPCRouter({
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
+  getYoutubeVideos: protectedProcedure.query(async ({ ctx }) => {
+    const accessToken = ctx.session.token.access_token;
+    try {
+      const options = {
+        part: "snippet",
+        maxResults: "50",
+        playlistId: "PLogYAbXxpcswCx7liCyjv05nGPggNiLOh",
+      };
+
+      const qs = new URLSearchParams(options);
+
+      const rootURL = "https://www.googleapis.com/youtube/v3/playlistItems";
+
+      const playlistItems = await fetch(`${rootURL}?${qs.toString()}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      return (await playlistItems.json()) as string;
+    } catch (error) {
+      console.log(error);
+    }
+  }),
 });
