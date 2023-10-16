1ac6b2d737e633520f4794cd332c05680d6340b3
riso.schwarcz <riso.schwarcz@gmail.com>
2023-10-16 21:38:17 +0200

    feat: rename file and add dark mode button

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Button } from "~/components/ui/button";
 import { api } from "~/utils/api";
 import Link from "next/link";
+import { ModeToggle } from "~/components/mode-toggle";
 
 function App() {
   const { status, data: sessionData } = useSession();
@@ -39,6 +40,8 @@ function App() {
   return (
     <div className="p-4">
       <p>Welcome {sessionData?.user.name}</p>
+      <ModeToggle />
+
       <div className="flex items-center justify-center gap-4">
         <Button
           onClick={() => {
@@ -91,6 +94,27 @@ function App() {
         {videos.map((video) => (
           <li key={video.etag}>{video.snippet.title}</li>
         ))}
+      </div>
+      <div className="mx-auto flex w-1/3 flex-col rounded-md border border-slate-600 p-4 text-justify">
+        <div className="mb-2">
+          &ldquo;Welcome to Notion Video Gallery! To begin your journey, we need
+          your permission to access essential features. Without authorization,
+          Notion Video Gallery won&apos;t function properly, and you won&apos;t
+          be able to use the app.
+        </div>
+
+        <div className="mb-2">
+          Your privacy and data security are our top priorities, and we only
+          request the permissions necessary for optimal performance.
+        </div>
+        <div className="mb-2">
+          To get started, click &lsquo;Next&rsquo; and experience the core
+          functionalities of Notion Video Gallery.
+        </div>
+        <div className="text-sm">
+          Thank you for choosing Notion Video Gallery—where every permission
+          ensures a smoother and more efficient user experience!&rdquo;
+        </div>
       </div>
     </div>
   );
