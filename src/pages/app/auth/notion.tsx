ad149d486eb8400dafdb467c52029dc4ab958cfc
riso.schwarcz <riso.schwarcz@gmail.com>
2023-10-16 21:39:38 +0200

    feat: create authorization pages after sign up

---
+++ src/pages/app/auth/notion.tsx
@@ -0,0 +1,10 @@
+function NotionAuthPage() {
+  return (
+    <div className="p-4">
+      <div className="text-2xl font-bold">Notion Auth Page</div>
+      <div>Get Notion OAuth tokens</div>
+    </div>
+  );
+}
+
+export default NotionAuthPage;
---
+++ src/pages/app/auth/youtube.tsx
@@ -0,0 +1,11 @@
+function YoutubeAuthPage() {
+  return (
+    <div className="p-4">
+      <div className="text-2xl font-bold">Youtube Auth Page</div>
+      <div>Next step of incremental authorization</div>
+      <div>Get Youtube OAuth tokens with youtube scope</div>
+    </div>
+  );
+}
+
+export default YoutubeAuthPage;
