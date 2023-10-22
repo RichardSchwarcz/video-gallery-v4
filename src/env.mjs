acc74597a1d4944c85855b7e8845d86c72835bd0
riso.schwarcz <riso.schwarcz@gmail.com>
2023-10-22 20:23:48 +0200

    feat: add some env variables for DB

--- src/env.mjs
+++ src/env.mjs
@@ -7,13 +7,7 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z
-      .string()
-      .url()
-      .refine(
-        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
-        "You forgot to change the default URL",
-      ),
+    POSTGRES_PRISMA_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -37,6 +31,8 @@ export const env = createEnv({
     NOTION_SNAPSHOT_ID: z.string().min(1),
     NOTION_CLIENT_ID: z.string().min(1),
     NOTION_CLIENT_SECRET: z.string().min(1),
+    NOTION_URI: z.string().min(1),
+    NOTION_REDIRECT_URI: z.string().min(1),
   },
 
   /**
@@ -53,7 +49,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
@@ -65,6 +61,8 @@ export const env = createEnv({
     NOTION_SNAPSHOT_ID: process.env.NOTION_SNAPSHOT_ID,
     NOTION_CLIENT_ID: process.env.NOTION_CLIENT_ID,
     NOTION_CLIENT_SECRET: process.env.NOTION_CLIENT_SECRET,
+    NOTION_URI: process.env.NOTION_URI,
+    NOTION_REDIRECT_URI: process.env.NOTION_REDIRECT_URI,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
