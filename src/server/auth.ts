4df5891b437d5f101d01f34f801c628551e7ff67
riso.schwarcz <riso.schwarcz@gmail.com>
2023-10-23 19:53:56 +0200

    feat: handle google incremental auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,5 +1,4 @@
-// import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { type GetServerSidePropsContext } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import {
   getServerSession,
   type DefaultSession,
@@ -9,7 +8,7 @@ import {
 import GoogleProvider from "next-auth/providers/google";
 
 import { env } from "~/env.mjs";
-// import { db } from "~/server/db";
+import { prisma } from "./db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -33,130 +32,184 @@ declare module "next-auth" {
   // }
 }
 
-// const GOOGLE_AUTHORIZATION_URL =
-
 /**
  * Takes a token, and returns a new token with updated
  * `accessToken` and `accessTokenExpires`. If an error occurs,
  * returns the old token and an error property
  */
-async function refreshAccessToken(token: TokenSet) {
-  try {
-    const url = "https://oauth2.googleapis.com/token";
-    if (token.refresh_token) {
-      const options = {
-        client_id: env.GOOGLE_CLIENT_ID,
-        client_secret: env.GOOGLE_CLIENT_SECRET,
-        grant_type: "refresh_token",
-        refresh_token: token.refresh_token,
-      };
-      const qs = new URLSearchParams(options);
+// async function refreshAccessToken(token: TokenSet) {
+//   try {
+//     const url = "https://oauth2.googleapis.com/token";
+//     if (token.refresh_token) {
+//       const options = {
+//         client_id: env.GOOGLE_CLIENT_ID,
+//         client_secret: env.GOOGLE_CLIENT_SECRET,
+//         grant_type: "refresh_token",
+//         refresh_token: token.refresh_token,
+//       };
+//       const qs = new URLSearchParams(options);
 
-      const response = await fetch(`${url}?${qs.toString()}`, {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        method: "POST",
-      });
+//       const response = await fetch(`${url}?${qs.toString()}`, {
+//         headers: {
+//           "Content-Type": "application/x-www-form-urlencoded",
+//         },
+//         method: "POST",
+//       });
 
-      const refreshedTokens = await response.json();
+//       const refreshedTokens = await response.json();
 
-      if (!response.ok) {
-        throw refreshedTokens;
-      }
+//       if (!response.ok) {
+//         throw refreshedTokens;
+//       }
 
-      return {
-        ...token,
-        access_token: refreshedTokens.access_token,
-        access_token_expires: Date.now() + refreshedTokens.expires_in * 1000,
-        refresh_token: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
-      };
-    }
-  } catch (error) {
-    console.log(error);
+//       return {
+//         ...token,
+//         access_token: refreshedTokens.access_token,
+//         access_token_expires: Date.now() + refreshedTokens.expires_in * 1000,
+//         refresh_token: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+//       };
+//     }
+//   } catch (error) {
+//     console.log(error);
 
-    return {
-      ...token,
-      error: "RefreshAccessTokenError",
-    };
-  }
-}
+//     return {
+//       ...token,
+//       error: "RefreshAccessTokenError",
+//     };
+//   }
+// }
 
-const scopes = [
-  "https://www.googleapis.com/auth/youtube",
-  "openid",
-  "email",
-  "profile",
-];
+// const rootURL = "https://accounts.google.com/o/oauth2/v2/auth";
 
-const rootURL = "https://accounts.google.com/o/oauth2/v2/auth";
+// const options = {
+//   access_type: "offline",
+//   response_type: "code",
+//   prompt: "consent",
+// };
 
-const options = {
-  access_type: "offline",
-  response_type: "code",
-  prompt: "consent",
-  // scope: scopes.join(" "),
-};
+// const qs = new URLSearchParams(options);
 
-const qs = new URLSearchParams(options);
+// const scopes = OAUTH_GOOGLE_SCOPES_WITHOUT_YOUTUBE;
+export function authOptionsWrapper(req: NextApiRequest, res: NextApiResponse) {
+  const authOptions: NextAuthOptions = {
+    session: {
+      strategy: "jwt",
+    },
+    callbacks: {
+      signIn: async ({ profile }) => {
+        /* 
+        Session is returned when user is signed in and is giving a permission for youtube scope
+        Session is needed to match youtube account to main account. Because these accounts could be different,
+        there is a risk of duplication.
+        */
+        const session = await getServerSession(req, res, authOptions);
+        console.log({ session });
 
-export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-  },
-  callbacks: {
-    jwt: ({ token, account }) => {
-      if (account) {
-        token = {
-          ...token,
-          access_token: account.access_token,
-          access_token_expires: account.expires_at,
-          refresh_token: account.refresh_token,
-          scope: account.scope,
-        };
+        const user = await prisma.user.findFirst({
+          where: { email: { contains: profile?.email } },
+        });
 
-        // Return previous token if the access token has not expired yet
-        if (Date.now() < account.expires_at! * 1000) {
-          return token;
+        if (profile) {
+          if (!user && !session) {
+            await prisma.user.create({
+              data: {
+                name: profile.name,
+                email: profile.email,
+                emailVerified: profile.email_verified,
+                picture: profile.picture,
+              },
+            });
+            return true;
+          }
+          if (!user && session) {
+            // find user id of main user which has active session
+            const mainUser = await prisma.user.findFirst({
+              where: { email: { contains: session.user.email } },
+            });
+            await prisma.youtubeAccount.create({
+              data: {
+                name: profile.name,
+                email: profile.email,
+                emailVerified: profile.email_verified,
+                picture: profile.picture,
+                userId: mainUser?.id,
+              },
+            });
+            return true;
+          }
+          return true;
         }
-        return refreshAccessToken(token);
-      }
-      return token;
-    },
-    session: ({ session, token }) => {
-      session = {
-        ...session,
-        token,
-      };
-      return session;
-    },
-    redirect: () => {
-      return "/app";
-    },
-  },
-  // adapter: PrismaAdapter(db),
-  providers: [
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        url: `${rootURL}?${qs.toString()}`,
-        params: {
-          scope: scopes.join(" "),
-        },
+        return true;
       },
-    }),
-  ],
-};
+      jwt: ({ token, account }) => {
+        if (account) {
+          token = {
+            ...token,
+            access_token: account.access_token,
+            access_token_expires: account.expires_at,
+            refresh_token: account.refresh_token,
+            scope: account.scope,
+          };
+
+          // Return previous token if the access token has not expired yet
+          if (Date.now() < account.expires_at! * 1000) {
+            return token;
+          }
+          // return refreshAccessToken(token);
+        }
+        return token;
+      },
+      session: ({ session, token }) => {
+        session = {
+          ...session,
+          token,
+        };
+        return session;
+      },
+      redirect: () => {
+        return "/app";
+      },
+    },
+    // adapter: PrismaAdapter(db),
+    providers: [
+      GoogleProvider({
+        clientId: env.GOOGLE_CLIENT_ID,
+        clientSecret: env.GOOGLE_CLIENT_SECRET,
+      }),
+    ],
+  };
+  return authOptions;
+}
 
 /**
  * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
  *
  * @see https://next-auth.js.org/configuration/nextjs
  */
-export const getServerAuthSession = (ctx: {
-  req: GetServerSidePropsContext["req"];
-  res: GetServerSidePropsContext["res"];
-}) => {
-  return getServerSession(ctx.req, ctx.res, authOptions);
-};
+// export const getServerAuthSession = (ctx: {
+//   req: GetServerSidePropsContext["req"];
+//   res: GetServerSidePropsContext["res"];
+// }) => {
+//   return getServerSession(
+//     ctx.req,
+//     ctx.res,
+//     // authOptionsWrapper(),
+//   );
+// };
+
+// interface GoogleProfile {
+//   iss: string;
+//   azp: string;
+//   aud: string;
+//   sub: string;
+//   email: string;
+//   email_verified: boolean;
+//   at_hash: string;
+//   name: string;
+//   picture: string;
+//   given_name: string;
+//   family_name: string;
+//   locale: string;
+//   iat: number;
+//   exp: number;
+// }
