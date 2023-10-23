412831709bb0393b8528b65de644e0bcf82e6c8e
riso.schwarcz <riso.schwarcz@gmail.com>
2023-10-23 20:02:14 +0200

    feat: add Link with empty href to be filled

--- src/pages/app/auth/notion.tsx
+++ src/pages/app/auth/notion.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 function NotionAuthPage() {
   return (
     <div className="p-4">
       <div className="text-2xl font-bold">Notion Auth Page</div>
+      {/* href env.NOTION_URI */}
+      <Link href={""}>Proceed for Authorization</Link>
+
       <div>Get Notion OAuth tokens</div>
     </div>
   );
