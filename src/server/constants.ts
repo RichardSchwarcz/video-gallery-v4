bbe21e2be217ae611318137a71e8bb0b59f31605
riso.schwarcz <riso.schwarcz@gmail.com>
2023-10-23 19:30:48 +0200

    feat: add google oauth scopes to constants file

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -1,1 +1,7 @@
 export const PLAYLIST_ID = "PLogYAbXxpcswCx7liCyjv05nGPggNiLOh";
+export const OAUTH_GOOGLE_SCOPES = [
+  "openid",
+  "email",
+  "profile",
+  "https://www.googleapis.com/auth/youtube",
+];
