f54a2bf44421ced38bcc9dcd3993eae865267ab9
riso.schwarcz <riso.schwarcz@gmail.com>
2023-10-25 20:15:33 +0200

    refactor: remove unused env vars

--- src/env.mjs
+++ src/env.mjs
@@ -11,28 +11,19 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
-    NEXTAUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string().min(1)
-        : z.string().min(1).optional(),
-    NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string().min(1) : z.string().url(),
-    ),
-    // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     GOOGLE_REDIRECT_URI: z.string().min(1),
-    NOTION_SECRET: z.string().min(1),
+
     NOTION_DATABASE_ID: z.string().min(1),
     NOTION_SNAPSHOT_ID: z.string().min(1),
+    NOTION_ACCESS_TOKEN: z.string().min(1),
+
     NOTION_CLIENT_ID: z.string().min(1),
     NOTION_CLIENT_SECRET: z.string().min(1),
-    NOTION_URI: z.string().min(1),
     NOTION_REDIRECT_URI: z.string().min(1),
+
+    NEXTAUTH_SECRET: z.string().min(1),
   },
 
   /**
@@ -51,18 +42,19 @@ export const env = createEnv({
   runtimeEnv: {
     POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
     NODE_ENV: process.env.NODE_ENV,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI,
-    NOTION_SECRET: process.env.NOTION_SECRET,
+
     NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
     NOTION_SNAPSHOT_ID: process.env.NOTION_SNAPSHOT_ID,
+    NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN,
+
     NOTION_CLIENT_ID: process.env.NOTION_CLIENT_ID,
     NOTION_CLIENT_SECRET: process.env.NOTION_CLIENT_SECRET,
-    NOTION_URI: process.env.NOTION_URI,
     NOTION_REDIRECT_URI: process.env.NOTION_REDIRECT_URI,
+
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
