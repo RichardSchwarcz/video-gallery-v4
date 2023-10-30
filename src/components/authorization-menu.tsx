0439e8a82812972215f80383d2d51e03d4b10e12
riso.schwarcz <riso.schwarcz@gmail.com>
2023-10-30 20:21:39 +0100

    feat: check if auth is needed

--- src/components/authorization-menu.tsx
+++ src/components/authorization-menu.tsx
@@ -11,13 +11,44 @@ import router from "next/router";
 import Notion from "./icons/notion";
 import Youtube from "./icons/youtube";
 import { TooltipWrapper } from "./tooltip-wrapper";
+import { cn } from "~/lib/utils";
+import type { Session } from "next-auth/core/types";
 
-export function AuthorizationMenu() {
+type PropsType = {
+  sessionData: Session | null;
+};
+
+export function AuthorizationMenu({ sessionData }: PropsType) {
+  const isYoutubeAuthorized = (sessionData: Session | null) => {
+    if (sessionData?.token.scope?.includes("youtube")) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  const isNotionAuthorized = () => {
+    return true;
+  };
+  const isAuthNeeded = (sessionData: Session | null) => {
+    if (isYoutubeAuthorized(sessionData) && isNotionAuthorized()) {
+      return false;
+    } else {
+      return true;
+    }
+  };
   return (
     <DropdownMenu>
       <TooltipWrapper text="Authorization">
         <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
+          <Button
+            variant="outline"
+            size="icon"
+            className={cn(
+              isAuthNeeded(sessionData)
+                ? "border-2 border-red-500 bg-red-100"
+                : "",
+            )}
+          >
             <Lock className="h-[1.2rem] w-[1.2rem]" />
             <span className="sr-only">Toggle theme</span>
           </Button>
@@ -28,15 +59,33 @@ export function AuthorizationMenu() {
           className="cursor-pointer"
           onClick={() => void router.push("/app/auth/youtube")}
         >
-          <Youtube />
-          <span className="px-2">Youtube Authorization</span>
+          <div className="flex w-full justify-between">
+            <div className="flex">
+              <Youtube />
+              <span className="px-2">Youtube Authorization</span>
+            </div>
+            {isYoutubeAuthorized(sessionData) ? (
+              <div className="h-[1rem] w-[1rem] rounded-full bg-green-700" />
+            ) : (
+              <div className="h-[1rem] w-[1rem] rounded-full bg-red-600" />
+            )}
+          </div>
         </DropdownMenuItem>
         <DropdownMenuItem
-          className="cursor-pointer"
+          className="cursor-pointer justify-between"
           onClick={() => void router.push("/app/auth/notion")}
         >
-          <Notion />
-          <span className="px-2">Notion Authorization</span>
+          <div className="flex w-full justify-between">
+            <div className="flex">
+              <Notion />
+              <span className="px-2">Notion Authorization</span>
+            </div>
+            {isNotionAuthorized() ? (
+              <div className="h-[1rem] w-[1rem] rounded-full bg-green-700" />
+            ) : (
+              <div className="h-[1rem] w-[1rem] rounded-full bg-red-600" />
+            )}
+          </div>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
