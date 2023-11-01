555ee0f5c7dea37e57d83e86924320aaf061dd85
riso.schwarcz <riso.schwarcz@gmail.com>
2023-11-01 19:56:07 +0100

    feat: move isYoutubeAuthorized to its own file

---
+++ src/utils/auth.ts
@@ -0,0 +1,9 @@
+import type { Session } from "next-auth";
+
+export const isYoutubeAuthorized = (sessionData: Session | null) => {
+  if (sessionData?.token.scope?.includes("youtube")) {
+    return true;
+  } else {
+    return false;
+  }
+};
