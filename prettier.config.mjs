3c8550702e426a1c833d7d374870bca2ac49921a
riso.schwarcz <riso.schwarcz@gmail.com>
2023-11-10 20:22:41 +0100

    style: single quotes, semi false

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,10 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
+/**
+ *  @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options & import("eslint-plugin-prettier")}
+ * */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+  plugins: ['prettier-plugin-tailwindcss', 'eslint-plugin-prettier'],
+  singleQuote: true,
+  semi: false,
+}
 
-export default config;
+export default config
