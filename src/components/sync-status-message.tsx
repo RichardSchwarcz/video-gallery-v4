140f4de14774ae5a72c294c9f3be3933a71ab481
riso.schwarcz <riso.schwarcz@gmail.com>
2023-11-11 20:40:52 +0100

    refactor: switch div circles for SVG

--- src/components/sync-status-message.tsx
+++ src/components/sync-status-message.tsx
@@ -1,10 +1,16 @@
 function SyncStatusMessage({ message }: { message: string }) {
   return (
     <div className="flex flex-col gap-1">
-      <div className="py-2">{message}</div>
-      <div className="mx-auto h-1 w-1 rounded-full border border-slate-700 bg-slate-500" />
-      <div className="mx-auto h-1 w-1 rounded-full border border-slate-700 bg-slate-500" />
-      <div className="mx-auto h-1 w-1 rounded-full border border-slate-700 bg-slate-500" />
+      <div className="rounded-md border-l-2 border-zinc-300 bg-zinc-100 p-2">
+        {message}
+      </div>
+      <div className="flex justify-center">
+        <svg height="20" width="33">
+          <circle cx="4" cy="8.5" r="3" fill="gray" />
+          <circle cx="14" cy="8.5" r="3" fill="gray" />
+          <circle cx="24" cy="8.5" r="3" fill="gray" />
+        </svg>
+      </div>
     </div>
   )
 }
