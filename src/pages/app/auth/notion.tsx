20b3a1c5970529ab1c84f6b44b0c735c688cc557
riso.schwarcz <riso.schwarcz@gmail.com>
2023-11-12 15:09:48 +0100

    feat: update notion auth page

--- src/pages/app/auth/notion.tsx
+++ src/pages/app/auth/notion.tsx
@@ -1,15 +1,69 @@
-import Link from "next/link";
+import Link from 'next/link'
+import Notion from '~/components/icons/notion'
+import { TooltipWrapper } from '~/components/tooltip-wrapper'
+import { Button } from '~/components/ui/button'
+import { api } from '~/utils/api'
 
 function NotionAuthPage() {
-  return (
-    <div className="p-4">
-      <div className="text-2xl font-bold">Notion Auth Page</div>
-      {/* href env.NOTION_URI */}
-      <Link href={""}>Proceed for Authorization</Link>
+  const { data } = api.notion.getOAuthURL.useQuery(undefined, {
+    enabled: true,
+  })
+  if (data) {
+    return (
+      <div className="p-4">
+        <div className="mx-auto flex w-1/3 flex-col rounded-md border border-slate-500 p-2">
+          <div className="flex items-center gap-2">
+            <Notion width="3rem" height="3rem" />
+            <div className="pb-2 text-center text-2xl font-bold">
+              Notion Authorization Consent
+            </div>
+          </div>
+          <div>
+            We need your consent to be able to access and sync your data with
+            Youtube.
+          </div>
+          <div className="pt-2">
+            With your consent we will be able to add youtube playlist items as
+            new pages to your Notion database.
+          </div>
+          <div className="pt-2">
+            For more information, please read how our sync works{' '}
+            <Link
+              href="/help#how-does-syncing-work"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              here
+            </Link>
+          </div>
+          <div className="flex justify-center gap-4 pt-8">
+            <Button asChild>
+              <Link href={data}>Continue</Link>
+            </Button>
+            <TooltipWrapper text="Go back to App">
+              <Button asChild variant={'outline'}>
+                <Link href="/app">App</Link>
+              </Button>
+            </TooltipWrapper>
+            <TooltipWrapper text="Go back to Home Page">
+              <Button asChild variant={'outline'}>
+                <Link href="/">Home Page</Link>
+              </Button>
+            </TooltipWrapper>
+          </div>
 
-      <div>Get Notion OAuth tokens</div>
-    </div>
-  );
+          <p className="py-2 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{' '}
+            <Link
+              href="/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Terms of Service
+            </Link>
+          </p>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default NotionAuthPage;
+export default NotionAuthPage
