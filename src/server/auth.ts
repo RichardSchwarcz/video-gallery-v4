62b3cd8d943e3ab5b1d546b4242ad0b80b8dbbb0
riso.schwarcz <riso.schwarcz@gmail.com>
2023-11-13 19:07:27 +0100

    feat: handle refresh token rotation

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,14 +1,14 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 import {
   getServerSession,
   type DefaultSession,
   type NextAuthOptions,
   type TokenSet,
-} from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+} from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 
-import { env } from "~/env.mjs";
-import { prisma } from "./db";
+import { env } from '~/env.mjs'
+import { prisma } from './db'
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -16,20 +16,15 @@ import { prisma } from "./db";
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session extends DefaultSession {
-    user: DefaultSession["user"] & {
-      id: string;
+    user: DefaultSession['user'] & {
+      id: string
       // ...other properties
       // role: UserRole;
-    };
-    token: TokenSet;
+    }
+    token: TokenSet
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
 /**
@@ -39,27 +34,27 @@ declare module "next-auth" {
  */
 async function refreshAccessToken(token: TokenSet) {
   try {
-    const url = "https://oauth2.googleapis.com/token";
+    const url = 'https://oauth2.googleapis.com/token'
     if (token.refresh_token) {
       const options = {
         client_id: env.GOOGLE_CLIENT_ID,
         client_secret: env.GOOGLE_CLIENT_SECRET,
-        grant_type: "refresh_token",
+        grant_type: 'refresh_token',
         refresh_token: token.refresh_token,
-      };
-      const qs = new URLSearchParams(options);
+      }
+      const qs = new URLSearchParams(options)
 
       const response = await fetch(`${url}?${qs.toString()}`, {
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
-        method: "POST",
-      });
+        method: 'POST',
+      })
 
-      const refreshedTokens = (await response.json()) as TokenSet;
+      const refreshedTokens = (await response.json()) as TokenSet
 
       if (!response.ok) {
-        throw refreshedTokens;
+        throw refreshedTokens
       }
 
       return {
@@ -67,37 +62,38 @@ async function refreshAccessToken(token: TokenSet) {
         access_token: refreshedTokens.access_token,
         access_token_expires: Date.now() + refreshedTokens.expires_at * 1000,
         refresh_token: refreshedTokens.refresh_token ?? token.refresh_token, // Fall back to old refresh token
-      };
+      }
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
 
     return {
       ...token,
-      error: "RefreshAccessTokenError",
-    };
+      error: 'RefreshAccessTokenError',
+    }
   }
 }
 
 export function authOptionsWrapper(req: NextApiRequest, res: NextApiResponse) {
   const authOptions: NextAuthOptions = {
     session: {
-      strategy: "jwt",
+      strategy: 'jwt',
     },
     callbacks: {
       signIn: async ({ profile }) => {
+        console.log({ profile })
         /* 
         Session is returned when user is signed in and is giving a permission for youtube scope
         Session is needed to match youtube account to main account. Because these accounts could be different,
         there is a risk of duplication.
         */
-        const session = await getServerSession(req, res, authOptions);
+        const session = await getServerSession(req, res, authOptions)
 
         // TODO uncomment when DB is available
         // const user = await prisma.user.findFirst({
         //   where: { email: { contains: profile?.email } },
         // });
-        const user = true;
+        const user = true
         // !!!---------------------------------------------------
 
         if (profile) {
@@ -109,14 +105,14 @@ export function authOptionsWrapper(req: NextApiRequest, res: NextApiResponse) {
                 emailVerified: profile.email_verified,
                 picture: profile.picture,
               },
-            });
-            return true;
+            })
+            return true
           }
           if (!user && session) {
             // find user id of main user which has active session
             const mainUser = await prisma.user.findFirst({
               where: { email: { contains: session.user.email } },
-            });
+            })
             await prisma.youtubeAccount.create({
               data: {
                 name: profile.name,
@@ -125,40 +121,38 @@ export function authOptionsWrapper(req: NextApiRequest, res: NextApiResponse) {
                 picture: profile.picture,
                 userId: mainUser?.id,
               },
-            });
-            return true;
+            })
+            return true
           }
-          return true;
+          return true
         }
-        return true;
+        return true
       },
       jwt: ({ token, account }) => {
         if (account) {
+          console.log({ account })
           token = {
             ...token,
             access_token: account.access_token,
             access_token_expires: account.expires_at,
             refresh_token: account.refresh_token,
             scope: account.scope,
-          };
-
-          // Return previous token if the access token has not expired yet
-          if (Date.now() < account.expires_at! * 1000) {
-            return token;
           }
-          return refreshAccessToken(token);
         }
-        return token;
+        if (Date.now() > token.access_token_expires) {
+          return refreshAccessToken(token)
+        }
+        return token
       },
       session: ({ session, token }) => {
         session = {
           ...session,
           token,
-        };
-        return session;
+        }
+        return session
       },
       redirect: () => {
-        return "/app";
+        return '/app'
       },
     },
     providers: [
@@ -167,8 +161,8 @@ export function authOptionsWrapper(req: NextApiRequest, res: NextApiResponse) {
         clientSecret: env.GOOGLE_CLIENT_SECRET,
       }),
     ],
-  };
-  return authOptions;
+  }
+  return authOptions
 }
 
 /**
@@ -177,15 +171,15 @@ export function authOptionsWrapper(req: NextApiRequest, res: NextApiResponse) {
  * @see https://next-auth.js.org/configuration/nextjs
  */
 export const getServerAuthSession = (ctx: {
-  req: NextApiRequest;
-  res: NextApiResponse;
+  req: NextApiRequest
+  res: NextApiResponse
 }) => {
   return getServerSession(
     ctx.req,
     ctx.res,
     authOptionsWrapper(ctx.req, ctx.res),
-  );
-};
+  )
+}
 
 // interface GoogleProfile {
 //   iss: string;
