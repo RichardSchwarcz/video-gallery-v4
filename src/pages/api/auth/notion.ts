2c7d55893345ba553c7441735618e6f0538fb4c0
riso.schwarcz <riso.schwarcz@gmail.com>
2023-11-15 19:25:24 +0100

    feat: check if user already has access token

--- src/pages/api/auth/notion.ts
+++ src/pages/api/auth/notion.ts
@@ -1,92 +1,107 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/react";
-import { env } from "~/env.mjs";
-import { prisma } from "~/server/db";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+import { env } from '~/env.mjs'
+import { prisma } from '~/server/db'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const code = req.query.code as string;
-  const session = await getSession({ req });
+  const code = req.query.code as string
+  const session = await getSession({ req })
   try {
     if (!session) {
-      res.redirect("/");
+      res.redirect('/')
     }
 
-    const tokens = await getNotionOAuthTokens(code);
-    console.log(tokens);
-    const email = session?.user.email;
+    const tokens = await getNotionOAuthTokens(code)
 
-    if (tokens && email) {
-      // TODO uncomment when DB is available again
-      // // get user by email
-      // const user = await prisma.user.findFirst({
-      //   where: {
-      //     email: { contains: email },
-      //   },
-      // });
-      // await prisma.notionToken.create({
-      //   data: {
-      //     access_token: tokens.access_token,
-      //     token_type: tokens.token_type,
-      //     bot_id: tokens.bot_id,
-      //     workspace_name: tokens.workspace_name,
-      //     workspace_icon: tokens.workspace_icon,
-      //     workspace_id: tokens.workspace_id,
-      //     owner_id: tokens.owner.user.id,
-      //     duplicated_template_id: tokens.duplicated_template_id,
-      //     request_id: tokens.request_id,
-      //     userId: user?.id,
-      //   },
-      // });
-      res.status(200).redirect("/app");
+    const user = await prisma.user.findFirst({
+      where: {
+        OR: [
+          {
+            youtubeAccount: {
+              email: session?.user.email,
+            },
+          },
+          {
+            email: session?.user.email,
+          },
+        ],
+      },
+      select: {
+        notionAccessTokens: true,
+        id: true,
+      },
+    })
+
+    if (tokens) {
+      if (user?.notionAccessTokens[0]?.access_token === tokens.access_token) {
+        res.status(200).redirect('/app')
+      }
+      if (user?.notionAccessTokens[0]?.access_token !== tokens.access_token) {
+        await prisma.notionToken.create({
+          data: {
+            access_token: tokens.access_token,
+            token_type: tokens.token_type,
+            bot_id: tokens.bot_id,
+            workspace_name: tokens.workspace_name,
+            workspace_icon: tokens.workspace_icon,
+            workspace_id: tokens.workspace_id,
+            owner_id: tokens.owner.user.id,
+            duplicated_template_id: tokens.duplicated_template_id,
+            request_id: tokens.request_id,
+            userId: user?.id,
+          },
+        })
+        res.status(200).redirect('/app')
+      }
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
 export async function getNotionOAuthTokens(
   code: string,
 ): Promise<NotionOAuthTokens | undefined> {
-  const clientId = env.NOTION_CLIENT_ID;
-  const clientSecret = env.NOTION_CLIENT_SECRET;
-  const redirectUri = env.NOTION_REDIRECT_URI;
+  const clientId = env.NOTION_CLIENT_ID
+  const clientSecret = env.NOTION_CLIENT_SECRET
+  const redirectUri = env.NOTION_REDIRECT_URI
 
   // encode in base 64
-  const encoded = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
+  const encoded = Buffer.from(`${clientId}:${clientSecret}`).toString('base64')
 
-  const response = await fetch("https://api.notion.com/v1/oauth/token", {
-    method: "POST",
+  const response = await fetch('https://api.notion.com/v1/oauth/token', {
+    method: 'POST',
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
       Authorization: `Basic ${encoded}`,
     },
     body: JSON.stringify({
-      grant_type: "authorization_code",
+      grant_type: 'authorization_code',
       code: code,
       redirect_uri: redirectUri,
     }),
-  });
-  return response.json() as Promise<NotionOAuthTokens>;
+  })
+  return response.json() as Promise<NotionOAuthTokens>
 }
 
 interface NotionOAuthTokens {
-  access_token: string;
-  token_type: string;
-  bot_id: string;
-  workspace_name: string;
-  workspace_icon: string;
-  workspace_id: string;
+  access_token: string
+  token_type: string
+  bot_id: string
+  workspace_name: string
+  workspace_icon: string
+  workspace_id: string
   owner: {
-    type: string;
+    type: string
     user: {
-      object: string;
-      id: string;
-    };
-  };
-  duplicated_template_id: string | null;
-  request_id: string;
+      object: string
+      id: string
+    }
+  }
+  duplicated_template_id: string | null
+  request_id: string
 }
