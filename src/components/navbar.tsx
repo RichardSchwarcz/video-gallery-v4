f4c37a7f29c11b456cb70eadb3a75c1b029f7e87
riso.schwarcz <riso.schwarcz@gmail.com>
2023-11-25 19:25:21 +0100

    feat: add padding, on logo click - redirect to app

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,11 +3,17 @@ import { ModeToggle } from './mode-toggle'
 import { ProfileDropdownMenu } from './profile-dropdown-menu'
 import { Button } from './ui/button'
 import Logo from './icons/logo'
+import { useRouter } from 'next/router'
 
 function Navbar({ sessionData }: { sessionData: Session | null }) {
+  const router = useRouter()
   return (
-    <nav className="flex justify-between">
-      <Button variant="outline" className="px-2">
+    <nav className="flex justify-between pb-4">
+      <Button
+        variant="outline"
+        className="px-2"
+        onClick={() => void router.push('/app')}
+      >
         <Logo />
         <span className="pl-2">Video Gallery</span>
       </Button>
