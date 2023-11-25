f444b21b9ff674260e6c001c8d5da1a62d415a61
riso.schwarcz <riso.schwarcz@gmail.com>
2023-11-25 19:01:51 +0100

    feat: add inter font family and box shadow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
+import { type Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
+import { type AppType } from 'next/app'
+import { api } from '~/utils/api'
+import '~/styles/globals.css'
+import { ThemeProvider } from '~/components/theme-provider'
+import { Inter } from 'next/font/google'
 
-import { api } from "~/utils/api";
-
-import "~/styles/globals.css";
-import { ThemeProvider } from "~/components/theme-provider";
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -19,10 +23,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
         enableSystem
         disableTransitionOnChange
       >
-        <Component {...pageProps} />
+        <main className={`${inter.variable} font-sans`}>
+          <Component {...pageProps} />
+        </main>
       </ThemeProvider>
     </SessionProvider>
-  );
-};
+  )
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
