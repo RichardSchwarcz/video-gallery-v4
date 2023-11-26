f2d82abfe1f9272396048918fc97844dd773bf0c
riso.schwarcz <riso.schwarcz@gmail.com>
2023-11-26 10:02:09 +0100

    fix: typo in svg properties

--- src/components/icons/help.tsx
+++ src/components/icons/help.tsx
@@ -3,11 +3,11 @@ function HelpIcon() {
     <svg
       fill="none"
       height="24"
-      shape-rendering="geometricPrecision"
+      shapeRendering="geometricPrecision"
       stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
       viewBox="0 0 24 24"
       width="24"
     >
