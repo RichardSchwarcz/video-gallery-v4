6ef6d5da628e64b06fa9f69fd07ae4a559d47970
riso.schwarcz <riso.schwarcz@gmail.com>
2023-12-04 21:25:23 +0100

    feat: add skeleton if data is loading

--- src/pages/app/settings/index.tsx
+++ src/pages/app/settings/index.tsx
@@ -4,11 +4,13 @@ import Navbar from '~/components/navbar'
 import React from 'react'
 import SettingsTabs from '~/components/settings-tabs'
 import SettingsForm from '~/components/settings-form'
+import FormFieldSkeleton from '~/components/form-field-skeleton'
+import { Skeleton } from '~/components/ui/skeleton'
 
 function Settings() {
   const { data: sessionData } = useSession()
   const [isBanner, setIsBanner] = React.useState(false)
-  const { mutate, isLoading } = api.settings.setIds.useMutation()
+  const { mutate, isLoading, isSuccess } = api.settings.setIds.useMutation()
   const {
     data,
     error,
@@ -22,11 +24,10 @@ function Settings() {
     if (error) {
       setIsBanner(true)
     }
-  }, [error])
-
-  if (isDataLoading) {
-    return <div>Loading...</div>
-  }
+    if (isSuccess) {
+      setIsBanner(false)
+    }
+  }, [error, isSuccess])
 
   return (
     <div className="container mx-auto pt-6">
@@ -41,7 +42,22 @@ function Settings() {
           )}
           <div className="ml-20 w-8/12 rounded-md border border-slate-300 p-6 shadow-messages">
             <div className="">
-              <SettingsForm data={data} mutate={mutate} isLoading={isLoading} />
+              {isDataLoading ? (
+                <div className="flex flex-col gap-8">
+                  <FormFieldSkeleton />
+                  <FormFieldSkeleton />
+                  <FormFieldSkeleton />
+                  <div className="flex flex-row-reverse pr-14">
+                    <Skeleton className="h-10 w-28" />
+                  </div>
+                </div>
+              ) : (
+                <SettingsForm
+                  data={data}
+                  mutate={mutate}
+                  isLoading={isLoading}
+                />
+              )}
             </div>
           </div>
         </div>
