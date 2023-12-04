1a75d2e01efac5c83cf18409d8240240c7397e43
riso.schwarcz <riso.schwarcz@gmail.com>
2023-12-04 19:16:30 +0100

    feat: get rid of serverSideProps, move form to its file

--- src/pages/app/settings/index.tsx
+++ src/pages/app/settings/index.tsx
@@ -1,59 +1,19 @@
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Form, FormField } from '~/components/ui/form'
-import { Button } from '~/components/ui/button'
 import { api } from '~/utils/api'
 import { useSession } from 'next-auth/react'
 import Navbar from '~/components/navbar'
-import FormItemWrapper from '~/components/form-item-wrapper'
-import { formSchema } from '~/lib/validations/form'
-import type { z } from 'zod'
-import { ButtonLoading } from '~/components/ui/button-loading'
 import React from 'react'
 import SettingsTabs from '~/components/settings-tabs'
-import { cn } from '~/lib/utils'
-import type {
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType,
-} from 'next'
-import { createServerSideHelpers } from '@trpc/react-query/server'
-import { appRouter } from '~/server/api/root'
-import { createTRPCContext } from '~/server/api/trpc'
-import { TRPCError } from '@trpc/server'
+import SettingsForm from '~/components/settings-form'
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const helpers = createServerSideHelpers({
-    router: appRouter,
-    ctx: await createTRPCContext({ req: context.req, res: context.res }),
-  })
-
-  try {
-    const prefetchedIds = await helpers.settings.getIds.fetch(undefined)
-    return {
-      props: {
-        prefetchedIds,
-      },
-    }
-  } catch (error: unknown) {
-    if (error instanceof TRPCError) {
-      return {
-        props: {
-          error: error.message,
-        },
-      }
-    }
-  }
-}
-
-type settingsIdsType = z.infer<typeof formSchema>
-
-function Settings(
-  props: InferGetServerSidePropsType<typeof getServerSideProps>,
-) {
+function Settings() {
   const { data: sessionData } = useSession()
   const [isBanner, setIsBanner] = React.useState(false)
   const { mutate, isLoading } = api.settings.setIds.useMutation()
-  const { error } = api.settings.getIds.useQuery(undefined, {
+  const {
+    data,
+    error,
+    isLoading: isDataLoading,
+  } = api.settings.getIds.useQuery(undefined, {
     refetchOnWindowFocus: false,
     retry: false,
   })
@@ -64,25 +24,8 @@ function Settings(
     }
   }, [error])
 
-  const form = useForm<settingsIdsType>({
-    resolver: zodResolver(formSchema),
-    defaultValues: props.prefetchedIds,
-  })
-
-  const settingsIds = props.prefetchedIds ?? {
-    youtubePlaylistId: '',
-    notionMainDbId: '',
-    notionSnapshotDbId: '',
-  }
-
-  function onSubmit(values: settingsIdsType) {
-    mutate(values)
-  }
-  const offsetElementWidth = (ids: settingsIdsType | undefined) => {
-    if (ids) {
-      return 'w-20'
-    }
-    return 'w-10'
+  if (isDataLoading) {
+    return <div>Loading...</div>
   }
 
   return (
@@ -98,55 +41,7 @@ function Settings(
           )}
           <div className="ml-20 w-8/12 rounded-md border border-slate-300 p-6 shadow-messages">
             <div className="">
-              <Form {...form}>
-                <form
-                  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-                  onSubmit={form.handleSubmit(onSubmit)}
-                >
-                  <FormField
-                    control={form.control}
-                    name="youtubePlaylistId"
-                    render={({ field }) => (
-                      <FormItemWrapper
-                        field={field}
-                        id={settingsIds.youtubePlaylistId}
-                      />
-                    )}
-                  />
-
-                  <FormField
-                    control={form.control}
-                    name="notionMainDbId"
-                    render={({ field }) => (
-                      <FormItemWrapper
-                        field={field}
-                        id={settingsIds.notionMainDbId}
-                      />
-                    )}
-                  />
-
-                  <FormField
-                    control={form.control}
-                    name="notionSnapshotDbId"
-                    render={({ field }) => (
-                      <FormItemWrapper
-                        field={field}
-                        id={settingsIds.notionSnapshotDbId}
-                      />
-                    )}
-                  />
-                  <div className="flex justify-end gap-4">
-                    {isLoading ? (
-                      <ButtonLoading loadingText="Please wait" />
-                    ) : (
-                      <Button type="submit">Save</Button>
-                    )}
-                    <div
-                      className={cn(offsetElementWidth(props.prefetchedIds))}
-                    />
-                  </div>
-                </form>
-              </Form>
+              <SettingsForm data={data} mutate={mutate} isLoading={isLoading} />
             </div>
           </div>
         </div>
