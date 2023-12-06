cc4411faf59c165f8ff63d57d21b55c9b26a231a
riso.schwarcz <riso.schwarcz@gmail.com>
2023-12-06 18:35:27 +0100

    chore: try catch in get notion token

--- src/server/api/routers/notionRouter.ts
+++ src/server/api/routers/notionRouter.ts
@@ -25,6 +25,7 @@ import {
 import { env } from '~/env.mjs'
 import { prisma } from '~/server/db'
 import { usersNotionAccessTokenSchema } from '~/lib/validations/user'
+import { TRPCError } from '@trpc/server'
 
 export const notionRouter = createTRPCRouter({
   handleInitialLoad: protectedProcedure.query(async ({ ctx }) => {
@@ -142,30 +143,47 @@ export const notionRouter = createTRPCRouter({
     return url
   }),
   getNotionToken: protectedProcedure.query(async ({ ctx }) => {
-    const user = await prisma.user.findFirst({
-      where: {
-        OR: [
-          {
-            youtubeAccount: {
+    try {
+      const user = await prisma.user.findFirst({
+        where: {
+          OR: [
+            {
+              youtubeAccount: {
+                email: ctx.session.user.email,
+              },
+            },
+            {
               email: ctx.session.user.email,
             },
-          },
-          {
-            email: ctx.session.user.email,
-          },
-        ],
-      },
-      select: {
-        notionAccessToken: true,
-      },
-    })
+          ],
+        },
+        select: {
+          notionAccessToken: true,
+        },
+      })
 
-    //TODO if no user, throw error
+      const result = usersNotionAccessTokenSchema.safeParse(
+        user?.notionAccessToken,
+      )
 
-    const usersNotionData = usersNotionAccessTokenSchema.parse(
-      user?.notionAccessToken,
-    )
-
-    return usersNotionData.access_token
+      if (result.success) {
+        return result.data.access_token
+      }
+      if (result.error) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'Notion token not found',
+        })
+      }
+    } catch (error) {
+      if (error instanceof TRPCError && error.code === 'NOT_FOUND') {
+        throw error
+      }
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: 'Something went wrong on our end',
+        cause: error,
+      })
+    }
   }),
 })
