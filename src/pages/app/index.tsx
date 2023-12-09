0b09ac8899b080ea2118f9866c54863ffd2727dd
riso.schwarcz <riso.schwarcz@gmail.com>
2023-12-09 19:44:59 +0100

    chore: remove reducer, render sync messages

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,125 +1,68 @@
 import { useSession } from 'next-auth/react'
-import { useReducer, useState } from 'react'
+import { useState } from 'react'
 import { Button } from '~/components/ui/button'
 import Link from 'next/link'
 import { ButtonLoading } from '~/components/ui/button-loading'
-import { type EventSourceDataType, syncMessage } from '../api/sync'
-import { initialState, reducer } from '~/utils/reducer'
+import {
+  type EventSourceMessages,
+  syncMessage,
+  type ResponseData,
+} from '../api/sync'
 import Navbar from '~/components/navbar'
 import SyncIcon from '~/components/icons/sync'
 import { api } from '~/utils/api'
 import WelcomeMessage from '~/components/welcome-message'
 import SettingsIcon from '~/components/icons/settings'
 import { Skeleton } from '~/components/ui/skeleton'
+import { formatDateObject } from '~/utils/formatDateObject'
 
 function App() {
+  const [message, setMessage] = useState<string>('')
+  const [syncData, setSyncData] = useState<ResponseData>({
+    newDataToSnapshotDB: [],
+    newDataToMainDB: [],
+    archivedVideoInfo: [],
+  })
   const { status, data: sessionData } = useSession()
   const [isSyncing, setIsSyncing] = useState(false)
-  const { mutate } = api.settings.setLastSync.useMutation()
+  const { mutate: lastSyncMutation } = api.settings.setLastSync.useMutation()
   const { data: lastSync } = api.settings.getLastSync.useQuery(undefined, {
     retry: false,
     refetchOnWindowFocus: false,
   })
-  const [state, dispatch] = useReducer(reducer, initialState)
   const { isSuccess: hasSettings, isLoading } =
     api.settings.hasSettings.useQuery(undefined, {
       retry: false,
       refetchOnWindowFocus: false,
     })
 
-  function formatDateTime(date: Date): string {
-    const day = date.getDate().toString().padStart(2, '0')
-    const month = (date.getMonth() + 1).toString().padStart(2, '0') // Months are zero-based
-    const year = date.getFullYear().toString()
-
-    const hours = date.getHours().toString().padStart(2, '0')
-    const minutes = date.getMinutes().toString().padStart(2, '0')
-    const seconds = date.getSeconds().toString().padStart(2, '0')
-
-    return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`
-  }
-
   const handleSync = () => {
     setIsSyncing(true)
     try {
       const eventSource = new EventSource('/api/sync')
       eventSource.addEventListener('syncEvent', (e) => {
-        const data = JSON.parse(e.data as string) as EventSourceDataType
-        if (data.message == syncMessage.deleted) {
-          dispatch({
-            ...initialState,
-            type: syncMessage.deleted,
-            deleted: { message: data.message, data: data.data },
-          })
-        }
-        if (data.message == syncMessage.added) {
-          dispatch({
-            ...initialState,
-            type: syncMessage.added,
-            added: { message: data.message, data: data.data },
-          })
-        }
-        if (data.message == syncMessage.synced) {
-          dispatch({
-            ...initialState,
-            type: syncMessage.synced,
-            synced: { message: data.message },
-          })
-        }
+        const data = JSON.parse(e.data as string) as
+          | EventSourceMessages
+          | {
+              message: typeof syncMessage.done
+              data: ResponseData
+            }
+        setMessage(data.message)
+
         if (data.message == syncMessage.done) {
-          dispatch({
-            ...initialState,
-            type: syncMessage.done,
-            done: { message: data.message },
-          })
+          setSyncData(data.data)
           setIsSyncing(false)
-          mutate()
-        }
-        if (data.message == syncMessage.snapshot) {
-          dispatch({
-            ...initialState,
-            type: syncMessage.snapshot,
-            snapshot: { message: data.message, data: data.data },
-          })
-        }
-        if (data.message == syncMessage.adding) {
-          dispatch({
-            ...initialState,
-            type: syncMessage.adding,
-            adding: { message: data.message },
-          })
-        }
-        if (data.message == syncMessage.comparing) {
-          dispatch({
-            ...initialState,
-            type: syncMessage.comparing,
-            comparing: { message: data.message },
-          })
-        }
-        if (data.message == syncMessage.deleting) {
-          dispatch({
-            ...initialState,
-            type: syncMessage.deleting,
-            deleting: { message: data.message },
-          })
-        }
-        if (data.message == syncMessage.snapshotAdding) {
-          dispatch({
-            ...initialState,
-            type: syncMessage.snapshotAdding,
-            snapshotAdding: { message: data.message },
-          })
+          lastSyncMutation()
         }
       })
-      eventSource.addEventListener('open', (e) => {
-        console.log('open', e)
+      eventSource.addEventListener('open', () => {
+        return
       })
-      eventSource.addEventListener('error', (e) => {
-        console.log(e)
+      eventSource.addEventListener('error', () => {
         eventSource.close()
       })
     } catch (error) {
-      console.log(error)
+      throw new Error('error')
     }
   }
 
@@ -148,10 +91,23 @@ function App() {
   }
 
   const renderMessage = () => {
+    if (message !== '') {
+      if (message == 'Everything is in sync 🎉') {
+        setTimeout(() => {
+          setMessage('')
+        }, 5000)
+      }
+      return (
+        <div className="mt-4 rounded-md border border-slate-300 p-2 shadow-messages">
+          <p className="px-16">{message}</p>
+        </div>
+      )
+    }
+
     if (hasSettings && lastSync) {
       return (
         <div className="mt-4 rounded-md border border-slate-300 p-2 shadow-messages">
-          <p className="px-16">Last Sync: {formatDateTime(lastSync)}</p>
+          <p className="px-16">Last Sync: {formatDateObject(lastSync)}</p>
         </div>
       )
     }
