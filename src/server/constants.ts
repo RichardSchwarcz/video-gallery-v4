f8c7c239012f3d29a75f1830db021eba23af9d95
riso.schwarcz <riso.schwarcz@gmail.com>
2023-12-10 15:47:19 +0100

    refactor: remove unused constant

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -1,7 +1,6 @@
-export const PLAYLIST_ID = "PLogYAbXxpcswCx7liCyjv05nGPggNiLOh";
 export const OAUTH_GOOGLE_SCOPES = [
-  "openid",
-  "email",
-  "profile",
-  "https://www.googleapis.com/auth/youtube",
-];
+  'openid',
+  'email',
+  'profile',
+  'https://www.googleapis.com/auth/youtube',
+]
