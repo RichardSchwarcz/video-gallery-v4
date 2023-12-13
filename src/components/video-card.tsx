94b4c0e83f4192f318e81df914b040e2f19cfd47
riso.schwarcz <riso.schwarcz@gmail.com>
2023-12-13 20:09:07 +0100

    chore: crop image, adjust text based on that

--- src/components/video-card.tsx
+++ src/components/video-card.tsx
@@ -5,25 +5,29 @@ import type { VideoSchema } from '~/server/api/types/videoTypes'
 
 function VideoCard({ data }: { data: VideoSchema }) {
   return (
-    <div className="my-2 flex h-[152px] w-[141px] flex-col rounded-md border border-slate-500">
-      <Image
-        src={data.thumbnail}
-        alt="img"
-        width="141"
-        height="80"
-        className="rounded-t-md"
-        quality={100}
-      />
-      <TooltipWrapper text={data.title}>
-        <a href={data.url} target="_blank">
-          <div className="p-2 text-sm">{truncateTitle(data.title)}</div>
-        </a>
-      </TooltipWrapper>
-      <TooltipWrapper text={data.videoOwnerChannelTitle}>
-        <div className="p-2 text-sm font-semibold">
-          {data.videoOwnerChannelTitle}
-        </div>
-      </TooltipWrapper>
+    <div className="relative my-2 flex h-[152px] w-[141px] flex-col rounded-md outline outline-1 outline-slate-500">
+      <div className="relative h-[78px] w-[141px] overflow-hidden rounded-t-md">
+        <Image
+          src={data.thumbnail}
+          alt="img"
+          width="141"
+          height="80"
+          className="absolute top-[-15px]"
+          quality={100}
+        />
+      </div>
+      <div className="absolute top-[80px] p-2">
+        <TooltipWrapper text={data.title}>
+          <a href={data.url} target="_blank">
+            <div className="text-sm">{truncateTitle(data.title, 30)}</div>
+          </a>
+        </TooltipWrapper>
+        <TooltipWrapper text={data.videoOwnerChannelTitle}>
+          <div className="text-sm font-semibold">
+            {data.videoOwnerChannelTitle}
+          </div>
+        </TooltipWrapper>
+      </div>
     </div>
   )
 }
