9d0ef0f59ec5e095baf5ed627f4ae8627f1fa0c3
riso.schwarcz <riso.schwarcz@gmail.com>
2023-12-13 19:15:46 +0100

    chore: create sync details layout

--- src/components/sync-details-tabs.tsx
+++ src/components/sync-details-tabs.tsx
@@ -1,12 +1,33 @@
 import { Button } from './ui/button'
 
-function SyncDetailsTabs() {
+type props = {
+  onClickSyncDetailsTab: (tab: 'added' | 'deleted' | 'errors' | 'hide') => void
+}
+
+function SyncDetailsTabs({ onClickSyncDetailsTab }: props) {
   return (
-    <div className="flex w-[140px] flex-col gap-4">
-      <Button variant={'outline'}>Added videos</Button>
-      <Button variant={'outline'}>Deleted Videos</Button>
-      <Button variant={'outline'}>Errors</Button>
-      <Button>Hide Details</Button>
+    <div className="fixed top-0 flex w-[140px] flex-col gap-4 pt-20">
+      <Button
+        variant={'outline'}
+        onClick={() => onClickSyncDetailsTab('added')}
+      >
+        Added videos
+      </Button>
+      <Button
+        variant={'outline'}
+        onClick={() => onClickSyncDetailsTab('deleted')}
+      >
+        Deleted Videos
+      </Button>
+      <Button
+        variant={'outline'}
+        onClick={() => onClickSyncDetailsTab('errors')}
+      >
+        Errors
+      </Button>
+      <Button onClick={() => onClickSyncDetailsTab('hide')}>
+        Hide Details
+      </Button>
     </div>
   )
 }
--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { useSession } from 'next-auth/react'
 import { useState } from 'react'
 import { Button } from '~/components/ui/button'
@@ -19,6 +18,8 @@ import { Skeleton } from '~/components/ui/skeleton'
 import { formatDateObject } from '~/utils/formatDateObject'
 import SyncDetailsTabs from '~/components/sync-details-tabs'
 import VideoCard from '~/components/video-card'
+import { z } from 'zod'
+import { MockAddedVideos, MockDeletedVideos } from '~/utils/mockData'
 
 function App() {
   const [message, setMessage] = useState<TSyncMessages>()
@@ -27,8 +28,12 @@ function App() {
     newDataToMainDB: [],
     archivedVideoInfo: [],
   })
-  const { status, data: sessionData } = useSession()
   const [isSyncing, setIsSyncing] = useState(false)
+  const [syncDetails, setSyncDetails] = useState<
+    'added' | 'deleted' | 'errors' | 'summary' | 'hide'
+  >('summary')
+
+  const { status, data: sessionData } = useSession()
   const { mutate: lastSyncMutation } = api.settings.setLastSync.useMutation()
   const { data: lastSync } = api.settings.getLastSync.useQuery(undefined, {
     retry: false,
@@ -72,11 +77,15 @@ function App() {
 
   const renderSyncButton = () => {
     if (isSyncing) {
-      return <ButtonLoading loadingText="Syncing" />
+      return (
+        <div className="mb-4">
+          <ButtonLoading loadingText="Syncing" />
+        </div>
+      )
     }
     if (!hasSettings && !isLoading) {
       return (
-        <Button asChild className="cursor-pointer">
+        <Button asChild className="mb-4 cursor-pointer">
           <div className="flex gap-2">
             <SettingsIcon />
             <Link href={'/app/settings'} className="font-semibold">
@@ -90,7 +99,7 @@ function App() {
       return
     }
     return (
-      <Button onClick={() => handleSync()} className="w-64">
+      <Button onClick={() => handleSync()} className="mb-4 w-64">
         <SyncIcon />
         <span className="pl-2 font-semibold">Sync</span>
       </Button>
@@ -100,21 +109,21 @@ function App() {
   const renderMessage = () => {
     if (message) {
       return (
-        <div className="mt-4 rounded-md border border-slate-300 p-2 shadow-messages">
+        <div className="rounded-md border border-slate-300 p-2 shadow-messages">
           <p className="px-16">{message}</p>
         </div>
       )
     }
     if (hasSettings && lastSync) {
       return (
-        <div className="mt-4 rounded-md border border-slate-300 p-2 shadow-messages">
+        <div className="rounded-md border border-slate-300 p-2 shadow-messages">
           <p className="px-16">Last Sync: {formatDateObject(lastSync)}</p>
         </div>
       )
     }
     if (hasSettings && !lastSync) {
       return (
-        <div className="mt-4 rounded-md border border-slate-300 p-2 shadow-messages">
+        <div className="rounded-md border border-slate-300 p-2 shadow-messages">
           <p className="line-clamp-2 px-4">
             Try adding some videos to your YouTube playlist and sync!
           </p>
@@ -141,25 +150,127 @@ function App() {
     )
   }
 
+  // validate syncData with zod
+  const syncDataSchema = z
+    .object({
+      newDataToSnapshotDB: z.array(z.object({})),
+      newDataToMainDB: z.array(
+        z.object({
+          videoId: z.string(),
+          title: z.string(),
+          thumbnail: z.string(),
+          url: z.string(),
+          videoOwnerChannelTitle: z.string(),
+          duration: z.string(),
+        }),
+      ),
+      archivedVideoInfo: z.array(
+        z.object({
+          author_name: z.string(),
+          author_url: z.string(),
+          height: z.number(),
+          html: z.string(),
+          provider_name: z.string(),
+          provider_url: z.string(),
+          thumbnail_height: z.number(),
+          thumbnail_url: z.string(),
+          thumbnail_width: z.number(),
+          title: z.string(),
+          type: z.string(),
+          url: z.string(),
+          version: z.string(),
+          width: z.number(),
+        }),
+      ),
+    })
+    .refine((data) => {
+      return (
+        data.newDataToSnapshotDB.length > 0 ||
+        data.newDataToMainDB.length > 0 ||
+        data.archivedVideoInfo.length > 0
+      )
+    })
+  // const validatedSyncData = syncDataSchema.safeParse(syncData)
+
+  type TValidatedSyncData = z.infer<typeof syncDataSchema>
+
+  const validatedSyncData = {
+    success: true,
+    data: {
+      newDataToMainDB: MockAddedVideos,
+      archivedVideoInfo: MockDeletedVideos,
+    },
+  }
+  const renderSyncDetails = (data: TValidatedSyncData) => {
+    console.log(syncDetails)
+    if (syncDetails == 'added') {
+      return (
+        <div className="flex flex-col gap-4">
+          {data.newDataToMainDB.map((video) => {
+            return <VideoCard data={video} key={video.videoId} />
+          })}
+        </div>
+      )
+    }
+    if (syncDetails == 'deleted') {
+      return (
+        <div className="flex flex-col gap-4">
+          {data.archivedVideoInfo.map((video) => {
+            return (
+              <div key={video.url}>
+                <p>{video.title}</p>
+                <p>{video.author_name}</p>
+              </div>
+            )
+          })}
+        </div>
+      )
+    }
+    if (syncDetails == 'errors') {
+      return <div>errors</div>
+    }
+    if (syncDetails == 'summary') {
+      return (
+        <>
+          <p>
+            Added {data.newDataToMainDB.length} new{' '}
+            {data.newDataToMainDB.length == 1 ? 'video' : 'videos'} to Notion
+            database
+          </p>
+          <p className="pt-2">
+            Removed {data.archivedVideoInfo.length}{' '}
+            {data.archivedVideoInfo.length == 1 ? 'video' : 'videos'} from
+            YouTube playlist
+          </p>
+        </>
+      )
+    }
+  }
+
+  const onClickSyncDetailsTab = (
+    tab: 'added' | 'deleted' | 'errors' | 'hide',
+  ) => {
+    setSyncDetails(tab)
+  }
+
   return (
     <div className="container mx-auto pt-6">
       <Navbar sessionData={sessionData} />
-      <div className="flex">
-        <SyncDetailsTabs />
-        <div className="mx-auto flex w-2/3 flex-col items-center">
-          <div>{renderSyncButton()}</div>
-          <div className="flex justify-center">
-            {isLoading ? (
-              <Skeleton className="mt-4 h-8 w-96" />
-            ) : (
-              renderMessage()
-            )}
-          </div>
-          <div className="mt-4 w-full rounded-md border border-slate-300 p-2 shadow-messages">
-            {syncData.newDataToMainDB.map((video) => (
-              <VideoCard key={video.url} data={video} />
-            ))}
-          </div>
+      <div className="mx-auto flex flex-col items-center">
+        <div>{renderSyncButton()}</div>
+        <div className="flex justify-center">
+          {isLoading ? <Skeleton className="mt-4 h-8 w-96" /> : renderMessage()}
+        </div>
+        <div className="w-full">
+          {validatedSyncData.success ? (
+            <div className="flex ">
+              <SyncDetailsTabs onClickSyncDetailsTab={onClickSyncDetailsTab} />
+
+              <div className="mx-auto mt-4 w-8/12 rounded-md border border-slate-300 p-4 shadow-messages">
+                {renderSyncDetails(validatedSyncData.data)}
+              </div>
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
