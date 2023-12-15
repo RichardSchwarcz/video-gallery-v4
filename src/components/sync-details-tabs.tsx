169985900c5710daae23e6bfb76e664256cf0900
riso.schwarcz <riso.schwarcz@gmail.com>
2023-12-15 17:57:20 +0100

    chore: create active function that returns style for active tab

--- src/components/sync-details-tabs.tsx
+++ src/components/sync-details-tabs.tsx
@@ -1,30 +1,33 @@
+import { cn } from '~/lib/utils'
 import { Button } from './ui/button'
+import type { SyncDetails } from '~/pages/app'
 
 type props = {
-  onClickSyncDetailsTab: (tab: 'added' | 'deleted' | 'errors' | 'hide') => void
+  onClickSyncDetailsTab: (tab: Exclude<SyncDetails, 'summary'>) => void
+  syncDetails: SyncDetails
 }
 
-function SyncDetailsTabs({ onClickSyncDetailsTab }: props) {
+function SyncDetailsTabs({ onClickSyncDetailsTab, syncDetails }: props) {
+  const active = () => {
+    return 'bg-gradient-to-r from-yellow-200 via-lime-200 to-stone-100 text-slate-700 font-semibold'
+  }
   return (
     <div className="fixed top-20 flex w-[140px] flex-col gap-4 ">
       <Button
         variant={'outline'}
         onClick={() => onClickSyncDetailsTab('added')}
+        className={cn(syncDetails == 'added' && active())}
       >
         Added videos
       </Button>
       <Button
         variant={'outline'}
         onClick={() => onClickSyncDetailsTab('deleted')}
+        className={cn(syncDetails == 'deleted' && active())}
       >
         Deleted Videos
       </Button>
-      <Button
-        variant={'outline'}
-        onClick={() => onClickSyncDetailsTab('errors')}
-      >
-        Errors
-      </Button>
+
       <Button onClick={() => onClickSyncDetailsTab('hide')}>
         Hide Details
       </Button>
