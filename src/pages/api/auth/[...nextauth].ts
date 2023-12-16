60589fd3220219da31bd3f614d8b5ddb1108f8e8
riso.schwarcz <riso.schwarcz@gmail.com>
2023-12-16 19:09:02 +0100

    feat: remove incremental authorization

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,11 @@
-import NextAuth from "next-auth";
-import type { NextApiRequest, NextApiResponse } from "next/types";
-import { authOptionsWrapper } from "~/server/auth";
+import NextAuth from 'next-auth'
+import type { NextApiRequest, NextApiResponse } from 'next/types'
+import { authOptions } from '~/server/auth'
 
 export default async function authWrapper(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return await NextAuth(req, res, authOptionsWrapper(req, res));
+  return await NextAuth(req, res, authOptions)
 }
